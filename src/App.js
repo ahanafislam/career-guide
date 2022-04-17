e7b436cf7aeca8dd590d47b20c5aa29d39c2e8f3
ahanafislam <ahanafislam@gmail.com>
2022-04-18 01:52:32 +0600

    Design Signup page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import Header from './Pages/Shared/Header/Header';
 import Login from './Pages/Login/Login/Login';
 import Footer from './Pages/Shared/Footer/Footer';
+import Register from './Pages/Login/Register/Register';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
       </Routes>
       <Footer></Footer>
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Container, Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link} from 'react-router-dom';
 
-const Login = () => {
-
+const Register = () => {
     return (
         <Container className='min-vh-100 d-flex justify-content-center'>
             <div>
-                <h2 className='brand-text text-center'>Log In</h2>
-                <div className="shadow p-5" style={{"width":"30rem"}}>
+                <h2 className='brand-text text-center'>Sign Up</h2>
+                <div className="shadow p-5 me-auto ms-auto" style={{"width":"30rem"}}>
                     <Form>
+                        <Form.Group className="mb-3" controlId="formBasicFullName">
+                            <Form.Label>Full Name</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Full Name" required/>
+                        </Form.Group>
+                        
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
                             <Form.Control type="email" placeholder="Enter email" required/>
@@ -19,16 +23,18 @@ const Login = () => {
                             <Form.Label>Password</Form.Label>
                             <Form.Control type="password" placeholder="Password" required/>
                         </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Accept Terms and Conditions" />
+                        </Form.Group>
                         <button className='brand-btn' type="submit">
-                            Log in
+                            Sign Up
                         </button>
-                        <p className='mt-2'>New to Career Guide? <Link to="/register" className='text-primary pe-auto text-decoration-none'>Please Register</Link> </p>
-                        <p>Forget Password? <button className='btn btn-link text-primary pe-auto text-decoration-none'>Reset Password</button> </p>
                     </Form>
+                    <p className='mt-2'>Already have an account? <Link to="/login" className='text-primary pe-auto text-decoration-none'>Please Login</Link></p>
                 </div>
             </div>
         </Container>
     );
 };
 
-export default Login;
+export default Register;
