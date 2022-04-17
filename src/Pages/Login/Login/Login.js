a576f76e378cbca564da2c8b6caccf51ea328281
ahanafislam <ahanafislam@gmail.com>
2022-04-18 02:24:53 +0600

    Add signup and login functionalaty

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,23 +1,58 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Container, Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
 
 const Login = () => {
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const navigate = useNavigate();
+
+    let errorElement;
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    
+    useEffect(() => {
+        user && navigate('/');
+    },[user, navigate]);
+
+    if(loading) {
+        console.log("loading...");
+    }
+
+    if(error) {
+        errorElement = <p className='text-danger'>{error?.message}</p>
+    }
+
+    const handleLogin = event => {
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+
+        signInWithEmailAndPassword(email,password);
+    }
 
     return (
         <Container className='min-vh-100 d-flex justify-content-center'>
             <div>
                 <h2 className='brand-text text-center'>Log In</h2>
                 <div className="shadow p-5" style={{"width":"30rem"}}>
-                    <Form>
+                    {errorElement}
+                    <Form onSubmit={handleLogin}>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" required/>
+                            <Form.Control ref={emailRef} type="email" placeholder="Enter email" required/>
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" required/>
+                            <Form.Control ref={passwordRef} type="password" placeholder="Password" required/>
                         </Form.Group>
                         <button className='brand-btn' type="submit">
                             Log in
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,32 +1,62 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { Container, Form } from 'react-bootstrap';
-import { Link} from 'react-router-dom';
+import { Link, useNavigate} from 'react-router-dom';
+import auth from '../../../firebase.init';
+import { useCreateUserWithEmailAndPassword, useUpdateProfile} from 'react-firebase-hooks/auth';
 
 const Register = () => {
+    const fullNameRef = useRef('');
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const navigate = useNavigate();
+    const [agree, setAgree] = useState(false);
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
+    const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+
+    const handleRegister = async event => {
+        event.preventDefault();
+        const fullName = fullNameRef.current.value;
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        
+        await createUserWithEmailAndPassword(email, password);
+        await updateProfile({ displayName: fullName });
+        navigate('/');
+    }
+
+    if(loading || updating) {
+        console.log("Loading..")
+    }
+
     return (
         <Container className='min-vh-100 d-flex justify-content-center'>
             <div>
                 <h2 className='brand-text text-center'>Sign Up</h2>
                 <div className="shadow p-5 me-auto ms-auto" style={{"width":"30rem"}}>
-                    <Form>
+                    <Form onSubmit={handleRegister}>
                         <Form.Group className="mb-3" controlId="formBasicFullName">
                             <Form.Label>Full Name</Form.Label>
-                            <Form.Control type="text" placeholder="Enter Full Name" required/>
+                            <Form.Control ref={fullNameRef} type="text" placeholder="Enter Full Name" required/>
                         </Form.Group>
                         
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" required/>
+                            <Form.Control ref={emailRef} type="email" placeholder="Enter email" required/>
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" required/>
+                            <Form.Control ref={passwordRef} type="password" placeholder="Password" required/>
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                            <Form.Check type="checkbox" label="Accept Terms and Conditions" />
+                            <Form.Check onClick={() => setAgree(!agree)} type="checkbox" label="Accept Terms and Conditions" />
                         </Form.Group>
-                        <button className='brand-btn' type="submit">
+                        <button disabled={!agree} className='brand-btn' type="submit">
                             Sign Up
                         </button>
                     </Form>
