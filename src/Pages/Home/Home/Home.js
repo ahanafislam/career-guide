c539126ad8e80292eec69e6e48e4c276ad059c3d
ahanafislam <ahanafislam@gmail.com>
2022-04-17 11:30:38 +0600

    Create Service.json file

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
-        <>  
+        <div>  
             <Banner></Banner>
-        </>
+            <Container>
+                <h1>I am home page.</h1>
+            </Container>
+        </div>
     );
 };
 
